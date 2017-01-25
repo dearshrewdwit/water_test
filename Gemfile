7bda739742cbeb16d54d254271bd030a5449ddcc
Edward Withers <edwardawithers@gmail.com>
2017-01-25 11:49:38 +0000

    set master to beginning

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
+ruby '2.3.3'
 # gem "rails"
  group :development, :test do
    gem 'rspec'
-   gem 'pry'
  end
--- lib/water.rb
+++ lib/water.rb
@@ -1,17 +1,5 @@
 class Water
-  def find_water(levels)
-    sections = find_sections(levels)
-    water_areas = sections.map { |section| levels[section] }
-    water_areas.map { |section| find_area(section) }.reduce(:+)
-  end
-
-  private
-
-  def find_sections(levels)
-    SectionFinder.new(levels).process([], 0, 0)
-  end
-
-  def find_area(section)
-    SectionArea.new(section).process
+  def find_water(data)
+    
   end
 end
