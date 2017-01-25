a808bff0ba699ae2b5d96d4d6f14fe7b95795271
Orest Hrycyna <orest.hrycyna@surfdome.com>
2017-01-25 11:55:30 +0000

    Solution

--- lib/water.rb
+++ lib/water.rb
@@ -1,5 +1,23 @@
 class Water
   def find_water(data)
-    
+    max_l = []
+    max_r = []
+    water = 0
+
+    for i in 0..levels.length-1 do
+      max_l[i] = [levels[i], (i != 0) ? max_l[i - 1] : 0].max
+    end
+
+    i = levels.length-1
+    while i >= 0 do
+      max_r[i] = [levels[i], i < (levels.length - 1) ? max_r[i + 1] : 0].max
+      i -= 1
+    end
+
+    for i in 1..levels.length-1 do
+      water += [max_l[i], max_r[i]].min - levels[i];
+    end
+
+    water
   end
 end
